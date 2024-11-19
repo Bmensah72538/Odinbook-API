3ba2c9c185d0f00fa5260be9898bffcc9c4510cb
Benjamin Mensah <Bmensah72538@gmail.com>
2024-11-18 19:24:48 -0600

    Port bound is now dynamic.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import cors from 'cors'
 
 // Init App
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const allowedOrigin = process.env.corsAllowedOrigin;
