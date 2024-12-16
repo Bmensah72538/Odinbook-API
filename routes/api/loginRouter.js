5058386212724d26fcec993b1691702c333a4150
Benjamin Mensah <bmensah72538@gmail.com>
2024-12-16 13:23:55 +0800

    Revised loginRouter

--- routes/api/loginRouter.js
+++ routes/api/loginRouter.js
@@ -3,25 +3,34 @@ import utils from '../../lib/passwordUtils.js';
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     //auth user
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({username: username})
-    .then((user) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({username: username});
+        // Does user exist?
         if(!user) {
-            res.send('invalid username/password');
+            res.json({error: 'Invalid username/password'});
+            return;
+        }
+        // Validate password
+        const validationResult = await utils.validPassword(password, user);
+        if(!validationResult) {
+            res.json({error: 'Invalid username/password'});
             return;
         }
         //then send JWT
-        const access = utils.issueAccess(user);
-        const refresh = utils.issueRefresh(user);
+        const access = await utils.issueAccess(user);
+        const refresh = await utils.issueRefresh(user);
         res.json({
             access: access,
             refresh: refresh,
             userId: user._id
         });
-    })
+    } catch (error) {
+        console.log(error);
+        res.json({error: 'Login failed.'});
+    }
     
 })
 
